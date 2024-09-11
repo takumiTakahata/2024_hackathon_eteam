e7328957c6c1eee2a696faec868a793d2a24cb67
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-09-11 14:04:06 +0900

    記事投稿確認コントローラー作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -13,4 +13,25 @@ class ArticleController extends Controller
         $tags = Tag::all();
         return view('article', ['tags' => $tags]);
     }
+
+    public function articleConfirm(Request $request)
+    {
+        // バリデーション
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required',
+            'tags' => 'required|array',
+        ]);
+
+        // タグ情報を取得
+        $tags = Tag::whereIn('id', $validatedData['tags'])->get();
+
+        // 確認画面にデータを渡す
+        return view('articleConfirm', [
+            'title' => $validatedData['title'],
+            'content' => $validatedData['content'],
+            'tags' => $tags,
+            'tag_ids' => $validatedData['tags'],
+        ]);
+    }
 }
