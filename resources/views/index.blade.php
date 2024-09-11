97dfc55629b4bdb3ff62c069fbd9a61a7484441e
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2024-09-11 11:38:41 +0900

    Topページの作成

---
+++ resources/views/index.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/top.css">
+    <title>ページTOP</title>
+</head>
+<body>
+    <header>
+        <p>アプリ名</p>
+    </header>
+    <main>
+        <p class="explanation">一人暮らしに苦労している方々の悩みの解決や生活を充実させることができるサイトです。</p>
+        <h1>おすすめの一人暮らしテクニックの記事</h1>
+        <article>
+            <p>記事タイトル1</p>
+            <p>投稿日</p>
+            <p>タグ</p>
+            <p>記事内容1</p>
+            <video controls width="250">
+                <source src="" type="video/webm"/>
+            </video> 
+        </article>
+        <h1>おすすめの一人暮らしの悩みを解決する記事</h1>
+        <article>
+            <p>記事タイトル2</p>
+            <p>投稿日</p>
+            <p>タグ</p>
+            <p>記事内容2</p>
+            <video controls width="250">
+                <source src="" type="video/webm"/>
+            </video> 
+        </article>
+    </main>
+    <footer>
+        <a href="">知恵袋投稿</a>
+        <a href="">投稿記事</a>
+        <a href="">知恵袋一覧</a>
+        <a href="">記事一覧</a>
+    </footer>
+</body>
+</html>
