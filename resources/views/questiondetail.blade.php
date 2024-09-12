4aef9960433c97f02f2a7e3d375891808480db79
Tomaki.s <s.tomaki.sys23@morijyobi.ac.jp>
2024-09-12 12:41:59 +0900

    知恵袋詳細表示

--- resources/views/questiondetail.blade.php
+++ resources/views/questiondetail.blade.php
@@ -1,16 +1,26 @@
+<!-- resources/views/questiondetail.blade.php -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>知恵袋詳細ページ</title>
+    <title>質問詳細</title>
 </head>
 <body>
-    <h1>知恵袋詳細ページ</h1>
+    <h1>質問詳細</h1>
 
-    <h2>{{$question->title}}</h2>
-    <p>{{$question=>text}}</p>
+    <div>
+        <h2>{{ $question->title }}</h2>
+        <p>{{ $question->text }}</p>
 
-    
+        <h3>関連タグ</h3>
+        <ul>
+            @forelse ($question->tags as $tag)
+                <li>{{ $tag->name }}</li>
+            @empty
+                <li>タグはありません</li>
+            @endforelse
+        </ul>
+    </div>
 </body>
 </html>
