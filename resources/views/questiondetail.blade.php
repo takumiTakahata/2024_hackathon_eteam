c3426746fbdc13bc7f02e6e89c21c37870692f60
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2024-09-17 12:16:59 +0900

    配置の調整

--- resources/views/questiondetail.blade.php
+++ resources/views/questiondetail.blade.php
@@ -14,7 +14,7 @@
             <p class="title">{{ $question->title }}</p>
             
             <h3>関連タグ</h3>
-            <ul class="tag-group">
+            <ul class="tag_group">
                 @forelse ($question->tags as $tag)
                 <li>{{ $tag->name }}</li>
                 @empty
@@ -23,15 +23,13 @@
             </ul>
             <p class="content">{{ $question->text }}</p>
         </div>
-
-        <h2>コメント</h2>
         <form action="{{ route('answer.store', $question->id) }}" method="POST">
             @csrf
             <textarea name="text" rows="2" cols="50" required placeholder="コメントを入力"></textarea>
             <button type="submit" class="btn btn-comment">コメントを投稿する</button>
         </form>
 
-        <h3>コメント一覧</h3>
+        <p class="comment_list">コメント一覧</p>
         @foreach ($question->answer as $answer)
         <div>
             <p class="comment">{{ $answer->text}}</p>
