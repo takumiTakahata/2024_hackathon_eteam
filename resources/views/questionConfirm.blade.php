4a3119c5b31ad57e0e65d4c90182f7eaadd6c540
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2024-09-17 11:18:22 +0900

    知恵袋投稿確認画面の作成

--- resources/views/questionConfirm.blade.php
+++ resources/views/questionConfirm.blade.php
@@ -9,36 +9,28 @@
 </head>
 <body>
   <main>
-    <form action="{{ route('question.add') }}" method="POST">
-      @csrf
-      <div class="form-group">
-        <label for="title">タイトル</label>
-        <p>{{ $title }}</p>
-        <input type="hidden" name="title" value="{{ $title }}">
-      </div>
-
-      <div class="form-group">
-        <label for="text">内容</label>
-        <p>{{ $text }}</p>
-        <input type="hidden" name="text" value="{{ $text }}">
-      </div>
-
-      <div class="form-group">
-        <label for="tags">タグ</label>
-        <ul>
-          @foreach ($tags as $tag)
-          <li>{{ $tag->name }}</li>
+    <p class="page_title">知恵袋確認</p>
+    <div class="confirm">
+      <form action="{{ route('question.add') }}" method="POST">
+        @csrf
+          <p class="title">{{ $title }}</p>
+          <input type="hidden" name="title" value="{{ $title }}">
+          <ul class="tag-group">
+            @foreach ($tags as $tag)
+            <li>{{ $tag->name }}</li>
+            @endforeach
+          </ul>
+          @foreach ($tag_ids as $tag_id)
+          <input type="hidden" name="tags[]" value="{{ $tag_id }}">
           @endforeach
-        </ul>
-        @foreach ($tag_ids as $tag_id)
-        <input type="hidden" name="tags[]" value="{{ $tag_id }}">
-        @endforeach
+          <p class="content">{{ $text }}</p>
+          <input type="hidden" name="text" value="{{ $text }}">
+    </div>
+      <div class="button">
+        <button type="submit" class="btn btn-success"><p>この知恵袋を投稿する</p></button>
+        <button><a href="" class="btn btn-return"><p>投稿画面に戻る</p></a></button>
       </div>
-
-      
-      <button type="submit" class="btn btn-success">この知恵袋を投稿する</button>
-
-    </form>
+      </form>
   </main>
 </body>
 </html>
