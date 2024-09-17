3da23a869d086701dc06ac9006f5527bd855f7b6
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2024-09-17 11:41:10 +0900

    確認画面の修正

--- resources/views/questionConfirm.blade.php
+++ resources/views/questionConfirm.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/reset.css">
-    <link rel="stylesheet" href="/css/question_confirm.css">
-    <title>Document</title>
-</head>
-<body>
+<link rel="stylesheet" href="/css/reset.css">
+<link rel="stylesheet" href="/css/question_confirm.css">
   <main>
     <p class="page_title">知恵袋確認</p>
     <div class="confirm">
@@ -32,5 +24,3 @@
       </div>
       </form>
   </main>
-</body>
-</html>
