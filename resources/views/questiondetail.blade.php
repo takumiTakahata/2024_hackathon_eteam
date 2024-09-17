d59bdf77928c38f506cfd0458cf166e0b6df624b
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2024-09-17 11:42:13 +0900

    質問部分の作成

--- resources/views/questiondetail.blade.php
+++ resources/views/questiondetail.blade.php
@@ -11,25 +11,24 @@
 <body>
     <main>
         <p class="page_title">質問詳細</p>
-
-        <div>
-            <h2>{{ $question->title }}</h2>
-            <p>{{ $question->text }}</p>
-
+        <div class="question">
+            <p class="title">{{ $question->title }}</p>
+            
             <h3>関連タグ</h3>
-            <ul>
+            <ul class="tag-group">
                 @forelse ($question->tags as $tag)
-                    <li>{{ $tag->name }}</li>
+                <li>{{ $tag->name }}</li>
                 @empty
-                    <li>タグはありません</li>
+                <li>タグはありません</li>
                 @endforelse
             </ul>
+            <p class="content">{{ $question->text }}</p>
         </div>
 
         <h2>コメント</h2>
         <form action="{{ route('answer.store', $question->id) }}" method="POST">
             @csrf
-            <textarea name="text" rows="4" cols="50" required></textarea>
+            <textarea name="text" rows="2" cols="50" required></textarea>
             <button type="submit">コメントを投稿する</button>
         </form>
 
