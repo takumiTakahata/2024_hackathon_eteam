39672c792d733ed81624fc9e47c29567182c0554
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2024-09-17 12:09:47 +0900

    ボタンの追加とプレースホルダーの作成

--- resources/views/questiondetail.blade.php
+++ resources/views/questiondetail.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -6,11 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/reset.css">
     <link rel="stylesheet" href="/css/question_detail.css">
-    <title>質問詳細</title>
+    <title>知恵袋詳細</title>
 </head>
 <body>
     <main>
-        <p class="page_title">質問詳細</p>
+        <p class="page_title">知恵袋詳細</p>
         <div class="question">
             <p class="title">{{ $question->title }}</p>
             
@@ -28,14 +27,14 @@
         <h2>コメント</h2>
         <form action="{{ route('answer.store', $question->id) }}" method="POST">
             @csrf
-            <textarea name="text" rows="2" cols="50" required></textarea>
-            <button type="submit">コメントを投稿する</button>
+            <textarea name="text" rows="2" cols="50" required placeholder="コメントを入力"></textarea>
+            <button type="submit" class="btn btn-comment">コメントを投稿する</button>
         </form>
 
         <h3>コメント一覧</h3>
         @foreach ($question->answer as $answer)
         <div>
-            <p>{{ $answer->text}}</p>
+            <p class="comment">{{ $answer->text}}</p>
         </div>
         @endforeach
     </main>
