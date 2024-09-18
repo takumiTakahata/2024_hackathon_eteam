70d49b4d6febb0f55e9d8d528b3578ba915c4598
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2024-09-18 12:27:23 +0900

    知恵袋投稿機能の画面の作成

--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -1,38 +1,51 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/add.css">
     <title>Document</title>
 </head>
 <body>
-<h1>知恵袋の投稿</h1>
-<form action="{{route('question.Confirm')}}" method="POST">
-  @csrf
-  <div class="form-group">
-    <label for="title">記事タイトル</label>
-    <input type="text" class="form-control" id="title" name="title" placeholder="タイトルを入力してください" required>
-  </div>
-
-  <div class="form-group">
-    <label for="tags">分類タグ</label>
-    <div>
-      @foreach ($tags as $tag)
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="tag{{ $tag->id }}" name="tags[]" value="{{ $tag->id }}">
-        <label class="form-check-label" for="tag{{ $tag->id }}">{{ $tag->name }}</label>
+  <main>
+    <p class="page_title">知恵袋の投稿</p>
+    <form action="{{route('question.Confirm')}}" method="POST">
+      @csrf
+      <!-- タイトル入力 -->
+      <div class="heading">
+        <p class="title">➤知恵袋タイトル</p>
+        <p class="caveat">必須</p>
       </div>
-      @endforeach
-    </div>
-  </div>
+      <input type="text" class="title_form" id="title" name="title" placeholder="タイトルを入力してください" required>
+      
+      <!-- タグ選択（複数可） -->
+      <div class="form-group">
+        <div class="heading">
+          <label for="tags" class="title">➤分類タグ</label>
+          <p class="caveat">必須</p>
+        </div>
+        <div class="tags_list">
+          @foreach ($tags as $tag)
+            <input type="checkbox" class="tags_form" id="tag{{ $tag->id }}" name="tags[]" value="{{ $tag->id }}">
+            <label class="check_label" for="tag{{ $tag->id }}">{{ $tag->name }}</label>
+          @endforeach
+        </div>
+      </div>
 
-  <div class="form-group">
-    <label for="text">投稿内容</label>
-    <textarea class="form-control" id="text" name="text" rows="5" placeholder="内容を入力してください" required></textarea>
-  </div>
-
-  <button type="submit" class="btn btn-primary">投稿</button>
-</form>
-    
+      <!-- 内容入力 -->
+      <div class="form-group">
+        <div class="heading">
+          <label for="content" class="title">➤投稿内容</label>
+          <p class="caveat">必須</p>
+        </div>
+        <textarea class="form-control" id="content" name="text" rows="5" placeholder="内容を入力してください" required></textarea>
+      </div>
+      <!-- 送信ボタン -->
+      <div>
+        <button type="submit" class="btn btn-primary">確認画面へ進む</button>
+      </div>
+    </form>
+  </main>
 </body>
 </html>
