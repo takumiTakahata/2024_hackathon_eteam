3c4dfee4b4e98f1d57cce180490f519b4f120f74
kenkensan <k.tanaka.sys22@morijyobi.ac.jp>
2024-09-19 11:11:58 +0900

    cssの追加

--- resources/views/questionindex.blade.php
+++ resources/views/questionindex.blade.php
@@ -4,6 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/list.css">
     <title>知恵袋一覧</title>
     <style>
         
