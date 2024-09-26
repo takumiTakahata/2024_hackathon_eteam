01767328d9401419b5dab16d768edf5c683b16ce
takumi <t.takahata.sys22@morijyobi.ac.jp>
2024-09-26 10:20:40 +0900

    アカウント登録画面への遷移をログイン画面に作成

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,11 +7,6 @@
 
         <!-- Email Address -->
         <div>
-            @if (session('message'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('message') }}
-            </div>
-            @endif
             <x-input-label for="email" :value="__('Email')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
@@ -39,6 +34,9 @@
 
         <div class="flex items-center justify-end mt-4">
             @if (Route::has('password.request'))
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 me-4" href="{{ route('register') }}">
+                {{ __('新規アカウント登録') }}
+            </a>
             <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                 {{ __('Forgot your password?') }}
             </a>
